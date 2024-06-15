2cd71473aae4b1fb57f5d5d5c21981c836d1c1a0
Chandrashekharsk <cs9981625252@gmail.com>
2024-06-15 19:37:51 +0530

    added home with logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand" to="/home">
             {props.title}{" "}
           </Link>
           {/* <a className="navbar-brand" href="#">
